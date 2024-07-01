76abb605cad2bfdeef5f15cf7ff1d8c94372fd97
George Province <provincegeorge412@gmail.com>
2024-07-02 00:00:47 +0530

    models

--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import ChaiVarity
+from django.shortcuts import get_object_or_404
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chai_data = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais' : chai_data})
+
+def chai_detail(request, chai_id):
+    
+    chai = get_object_or_404(ChaiVarity, pk =chai_id)
+    return render(request, 'chai/chai_detail.html', {'chai' : chai})
